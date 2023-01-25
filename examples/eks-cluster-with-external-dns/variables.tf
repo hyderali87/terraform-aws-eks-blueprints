ac46f42e6fcf879360a0d4b045698dd5dab3c46e
hyderali87 <34377320+hyderali87@users.noreply.github.com>
2023-01-25 15:02:23 +0530

    Update variables.tf

--- examples/eks-cluster-with-external-dns/variables.tf
+++ examples/eks-cluster-with-external-dns/variables.tf
@@ -1,11 +1,11 @@
 variable "eks_cluster_domain" {
   type        = string
   description = "Route53 domain for the cluster."
-  default     = "example.com"
+  default     = "cxupexample.com"
 }
 
 variable "acm_certificate_domain" {
   type        = string
   description = "Route53 certificate domain"
-  default     = "*.example.com"
+  default     = "*.cxupexample.com"
 }
